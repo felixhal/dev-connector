ffa3ed1d91447641ff699b9ad5743ab6441192fd
jackGiant24 <felix240600@gmail.com>
2020-11-03 23:33:09 +0700

    createProfile Page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-form/createProfile';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -34,6 +35,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
